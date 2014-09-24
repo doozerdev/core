4b28c3e3a179a8938fd3fb3ee1985c23c8838fd5
Joe Basirico <joebasirico@gmail.com>
2014-09-24 14:52:46 -0700

    a

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,2 +1,12 @@
-MongoMapper.connection = Mongo::Connection.new(ENV['MONGOHQ_URL'])
-MongoMapper.database = "#myapp-#{Rails.env}"
+config = YAML.load_file(Rails.root + 'config' + 'mongo.yml')[Rails.env]
+
+# MongoMapper.connection = Mongo::Connection.new(ENV['MONGOHQ_URL'])
+MongoMapper.connection = Mongo::Connection.new(config['host'], config['port'], {
+  :logger         => Rails.logger
+})
+
+#MongoMapper.database = "#myapp-#{Rails.env}"
+MongoMapper.database = config['database']
+if config['username'].present?
+  MongoMapper.database.authenticate(config['username'], config['password'])
+end
