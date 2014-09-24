aa5715f79beaefb8a4a49c4e31b861f13e6bbd90
Joe Basirico <joebasirico@gmail.com>
2014-09-24 13:17:40 -0700

    a

--- config/initializers/mongo.rb
+++
@@ -1,7 +0,0 @@
-if Rails.env.production?
-  MongoMapper.connection = Mongo::Connection.new(ENV['MONGOHQ_URL'])
-else
-  MongoMapper.connection = Mongo::Connection.new('localhost', 27017)  
-end
-
-MongoMapper.database = "#myapp-#{Rails.env}"
