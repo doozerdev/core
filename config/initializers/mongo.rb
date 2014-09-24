5a63915d2c18d7c02dd77bd174ae960a1e73d2dc
Joe Basirico <joebasirico@gmail.com>
2014-09-23 22:32:38 -0700

    more heroku

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,2 +1,7 @@
-MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
+if Rails.env.production?
+  MongoMapper.connection = Mongo::Connection.new('localhost', 27017)  
+else
+  MongoMapper.connection = Mongo::Connection.new(ENV['MONGOHQ_URL'])
+end
+
 MongoMapper.database = "#myapp-#{Rails.env}"
