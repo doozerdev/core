51a67ed5ed94ebc01a6eca633bb7cfe69ec9dced
Joe Basirico <joebasirico@gmail.com>
2014-10-12 15:21:50 -0700

    Fixed MongoDB connection for Compose and Heroku

--- README.md
+++ README.md
@@ -1,12 +1,33 @@
-DoozerCore
-====
+#DoozerCore
 
-Start with Ruby 2.1.0 and Rails 4.0.1.
+Start with the latest version of Ruby and Rails 4.1.1.
 http://rubyonrails.org/download/
 
 I believe everything will be installed with `bundle install` including mongo
 
+
+##Mongo Install
+
+###Local Mongo
+If you want to run a local mongo instance (I suggest this for all dev work)
+
 run monogodb by creating /data/db in your root dir and running `mongod`
 
-run the web server using the `rails c` command
+###Compose (hosted Mongo)
+`source .doozer_rc` from the root directory to load the MONGO_DB environment var
 
+In the config/mongo.yml file comment out the following lines.
+
+    <<: *defaults
+    database: doozer_development
+
+Uncomment or add the following lines (you need both)
+
+    uri: <%= ENV['MONGO_URL'] %>
+    use_env_var: true
+
+##Run
+
+Run the web server using the `rails c` command
+
+Browse to http://localhost:3000 (or whatever your local rails server is configured for)
--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,12 +1,36 @@
 config = YAML.load_file(Rails.root + 'config' + 'mongo.yml')[Rails.env]
 
-# MongoMapper.connection = Mongo::Connection.new(ENV['MONGOHQ_URL'])
-MongoMapper.connection = Mongo::Connection.new(config['host'], config['port'], {
-  :logger         => Rails.logger
-})
-
-#MongoMapper.database = "#myapp-#{Rails.env}"
-MongoMapper.database = config['database']
-if config['username'].present?
-  MongoMapper.database.authenticate(config['username'], config['password'])
+if config['use_env_var']
+  db = URI.parse(ENV['MONGO_URL'])
+  db_name = db.path.gsub(/^\//, '')
+  db_connection = Mongo::Connection.new(db.host, db.port).db(db_name)
+  db_connection.authenticate(db.user, db.password) unless (db.user.nil? || db.password.nil?)
+  MongoMapper.connection =db_connection
+else
+  MongoMapper.connection = Mongo::Connection.new(config['host'], config['port'], { :logger => Rails.logger })
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# MongoMapper.connection = Mongo::Connection.new(config['host'], config['port'], {
+#   :logger         => Rails.logger
+# })
+
+# #MongoMapper.database = "#myapp-#{Rails.env}"
+# MongoMapper.database = config['database']
+# if config['username'].present?
+#   MongoMapper.database.authenticate(config['username'], config['password'])
+# end
