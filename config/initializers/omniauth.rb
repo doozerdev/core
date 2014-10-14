ade0f459f26dc8c8db6d36d7b013e582e320d8f9
Joe Basirico <joebasirico@gmail.com>
2014-10-13 18:44:59 -0700

    get FB ID and Secret out of source

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '1474823829455959', 'b59ebb250ba214f7fc49f2f970de9989'
+  provider :facebook, ENV['FB_APP_ID'], ENV['FB_APP_SECRET']
 end
